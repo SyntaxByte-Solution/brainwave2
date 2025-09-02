cfb9eb0c5d6d377ec2c3f1c555284f60a5a53a0a
ademola123456 <68793270+ademola123456@users.noreply.github.com>
2025-09-02 18:31:00 +0100

    resources updates

--- src/app/(resources)/resource-details/[id]/page.jsx
+++ src/app/(resources)/resource-details/[id]/page.jsx
@@ -1,17 +1,20 @@
 import Header2 from "@/components/headers/Header2";
 import Newsletter from "@/components/blog/Newsletter";
-import Footer1 from "@/components/footers/Footer1";
-import BlogDetails from "@/components/Resources/ResourcesDetails";
+import Footer2 from "@/components/footers/Footer2";
+import ResourceDetails from "@/components/Resources/ResourcesDetails";
 import { allBlogs } from "@/data/blogs";
 export const metadata = {
-  title:
-    "Resources Details || Brainwave ",
+  title: "Resources Details || Brainwave ",
   description:
     "Brainwave - Full-featured, professional-looking software, saas and startup nextjs template.",
 };
 export default function BlogDetailsPage1({ params }) {
   const id = params.id;
+
   const blogItem = allBlogs.filter((elm) => elm.id == id)[0] || allBlogs[0];
+  if (!blogItem) {
+    return <div className="panel py-10"> Article Not Found.</div>;
+  }
   return (
     <>
       <div className="page-wrapper uni-body panel bg-white text-gray-900 dark:bg-gray-900 dark:text-gray-200 overflow-x-hidden bp-xs bp-sm bp-md bp-lg bp-xl bp-xxl dom-ready">
@@ -20,9 +23,9 @@ export default function BlogDetailsPage1({ params }) {
           id="wrapper"
           className="wrap"
         >
-          <BlogDetails blogItem={blogItem} />
+          <ResourceDetails blogItem={blogItem} />
         </div>
-        <Footer1 />
+        <Footer2 />
       </div>
     </>
   );
