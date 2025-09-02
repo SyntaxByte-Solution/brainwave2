1b14477e86edf9e7443f68c03c171415acca4a15
ademola123456 <68793270+ademola123456@users.noreply.github.com>
2025-09-02 18:53:12 +0100

    Update Blog

--- src/app/components/homes/home-3/Blog.jsx
+++ src/app/components/homes/home-3/Blog.jsx
@@ -1,4 +1,4 @@
-import { blogPosts } from "@/data/blogs";
+import { blogsPosts4 } from "@/data/blogs";
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
@@ -40,12 +40,12 @@ export default function Blog() {
 
               <div className="panel">
                 <div className="row child-cols-12 sm:child-cols-6 lg:child-cols-4 justify-center g-2 xl:g-4">
-                  {blogPosts.map((post, index) => (
+                  {blogsPosts4.map((post, index) => (
                     <div key={index}>
                       <article className="post type-post panel vstack gap-3 rounded-3 p-2 pb-3 bg-secondary dark:bg-gray-800">
                         <Link
                           className="position-absolute top-0 ltr:start-0 rtl:end-0 m-3 fs-7 fw-bold text-none z-1 bg-primary text-white py-narrow px-1"
-                          href={`/blog`}
+                          href={`/resource`}
                           style={{ borderRadius: 8 }}
                         >
                           {post.category}
@@ -53,51 +53,26 @@ export default function Blog() {
                         <figure className="featured-image m-0 rounded ratio ratio-3x2 rounded-2 uc-transition-toggle overflow-hidden">
                           <Image
                             className="media-cover image uc-transition-scale-up uc-transition-opaque"
-                            src={post.imageSrc}
+                            src={post.imgSrc}
                             width={1280}
                             height={854}
-                            alt={post.imageAlt}
+                            alt={post.imgAlt}
                           />
                           <Link
-                            href={`/blog-details/${post.id}`}
+                            href={`/resource-details/${post.id}`}
                             className="position-cover"
-                            data-caption={post.imageAlt}
+                            data-caption={post.imgAlt}
                           ></Link>
                         </figure>
                         <header className="panel vstack items-center   gap-1 lg:gap-2 px-2 py-4 border-gray-100 dark:border-gray-900 dark:bg-gray-900">
                           <h3 className="h5 xl:h4 m-0 text-center m-0">
                             <Link
                               className="text-none"
-                              href={`/blog-details/${post.id}`}
+                              href={`/resource-details/${post.id}`}
                             >
                               {post.title}
                             </Link>
                           </h3>
-                          {/* <ul className="post-meta nav-x ft-tertiary justify-center gap-1 fs-7 text-gray-400 dark:text-gray-300 d-none lg:d-flex">
-                            <li>
-                              <div className="hstack gap-narrow ft-tertiary">
-                                <Image
-                                  src={post.authorImgSrc}
-                                  width={150}
-                                  height={150}
-                                  alt={post.authorName}
-                                  className="w-24px h-24px rounded-circle me-narrow"
-                                />
-                                <Link
-                                  href={`/blog-author/Amir Khan`}
-                                  className="text-none fw-bold text-dark dark:text-white"
-                                >
-                                  {post.authorName}
-                                </Link>
-                              </div>
-                            </li>
-                            <li className="opacity-50">•</li>
-                            <li>
-                              <div className="post-date hstack gap-narrow">
-                                <span>{post.date}</span>
-                              </div>
-                            </li>
-                          </ul> */}
                         </header>
                       </article>
                     </div>
